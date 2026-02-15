6e606271b8c8ff77744fb6450f4f04047d2ca102
MURADLAPTOP\Md Murad Hosen <muradbd.info@gmail.com>
2026-02-15 21:56:03 +0600

    Basic Laravel 11 with vue uploaded.

---
+++ resources/js/app.js
@@ -0,0 +1,43 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
+
+// PrimeVue CSS
+import 'primeicons/primeicons.css';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Easy School ERP';
+const pinia = createPinia();
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(pinia)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura,
+                    options: {
+                        prefix: 'p',
+                        darkModeSelector: '.dark',
+                        cssLayer: false
+                    }
+                }
+            })
+            .use(ToastService)
+            .use(ConfirmationService)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
